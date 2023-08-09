6cc927b9c2faacba0505acdc2e1e3f5f69ceb5b3
peppone-choi <peppone.choi@gmail.com>
2023-08-09 20:16:21 +0900

    Feat: 프로젝트 세팅
    https://start.spring.io/에서 프로젝트 세팅

---
+++ queosk/src/main/java/com/bttf/queosk/QueoskApplication.java
@@ -0,0 +1,13 @@
+package com.bttf.queosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueoskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueoskApplication.class, args);
+	}
+
+}
---
+++ queosk/src/test/java/com/bttf/queosk/QueoskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bttf.queosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueoskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
