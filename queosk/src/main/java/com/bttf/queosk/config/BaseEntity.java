9f7985fe8fecd7c2e60f2e12b195560429110729
peppone-choi <peppone.choi@gmail.com>
2023-08-11 11:53:05 +0900

    Feat: BaseEntity 적용을 위한 세팅

--- queosk/src/main/java/com/bttf/queosk/config/BaseEntity.java
+++ queosk/src/main/java/com/bttf/queosk/config/BaseEntity.java
@@ -1,13 +1,11 @@
 package com.bttf.queosk.config;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
