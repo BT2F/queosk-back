3b4d59a25c97d6fd9b37e8c3332d899381348461
peppone-choi <peppone.choi@gmail.com>
2023-08-13 09:21:23 +0900

    Feat: review 테이블 추가
    ReviewEntity, ReviewRepository 추가
    해당 리포지토리에 대한 테스트

---
+++ queosk/src/test/java/com/bttf/queosk/repository/ReviewRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.bttf.queosk.repository;
+
+import com.bttf.queosk.config.JpaAuditingConfiguration;
+import com.bttf.queosk.domain.enumerate.UserStatus;
+import com.bttf.queosk.entity.RestaurantEntity;
+import com.bttf.queosk.entity.ReviewEntity;
+import com.bttf.queosk.entity.UserEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Import(JpaAuditingConfiguration.class)
+@DataJpaTest
+class ReviewRepositoryTest {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void MenuRepository_test() throws Exception {
+        // given
+        UserEntity user = UserEntity.builder()
+                .id(1L)
+                .userId("test")
+                .email("a@x.com")
+                .password("test")
+                .phone("0100000000")
+                .status(UserStatus.NOT_VERIFIED)
+                .build();
+
+        userRepository.save(user);
+
+        RestaurantEntity restaurant = RestaurantEntity.builder()
+                .id(1L)
+                .ownerId("test")
+                .name("test")
+                .password("asd")
+                .build();
+
+        restaurantRepository.save(restaurant);
+
+        ReviewEntity review = ReviewEntity.builder()
+                .id(1L)
+                .restaurant(restaurant)
+                .user(user)
+                .content("test")
+                .subject("노맛")
+                .rate(1.2)
+                .build();
+        // when
+
+        reviewRepository.save(review);
+
+        // then
+
+        assertThat(reviewRepository.count()).isEqualTo(1);
+        assertThat(reviewRepository.findById(review.getId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 id의 리뷰가 없습니다.")
+        ).getSubject()).isEqualTo(review.getSubject());
+        assertThat(reviewRepository.findById(review.getId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 id의 리뷰가 없습니다.")
+        ).getContent()).isEqualTo(review.getContent());
+        assertThat(reviewRepository.findById(review.getId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 id의 리뷰가 없습니다.")
+        ).getRate()).isEqualTo(review.getRate());
+
+
+    }
+
+}
