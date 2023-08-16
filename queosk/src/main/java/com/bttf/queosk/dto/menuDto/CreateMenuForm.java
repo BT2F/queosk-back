03a0bfa413a06a2725e4a15f3217a7b05caed86e
peppone-choi <peppone.choi@gmail.com>
2023-08-16 10:26:21 +0900

    Feat: 메뉴 추가 기능 구현

---
+++ queosk/src/main/java/com/bttf/queosk/dto/menuDto/CreateMenuForm.java
@@ -0,0 +1,23 @@
+package com.bttf.queosk.dto.menuDto;
+
+import com.bttf.queosk.dto.enumerate.MenuStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMenuForm {
+    @NotEmpty(message = "이름은 비워둘 수 없습니다.")
+    private String name;
+    private MultipartFile imageUrl;
+    @NotEmpty(message = "가격은 비워둘 수 없습니다.")
+    private Long price;
+    private MenuStatus status;
+}
