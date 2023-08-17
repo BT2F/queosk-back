bf74c15ab7a8753977d441772e73711bbee829e2
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-08-17 18:50:23 +0900

    Rename: 패키지 구조 개선을 위한 패키지 리네이밍 (#33)

--- queosk/src/main/java/com/bttf/queosk/entity/Table.java
+++ queosk/src/main/java/com/bttf/queosk/entity/Table.java
@@ -1,7 +1,7 @@
 package com.bttf.queosk.entity;
 
 import com.bttf.queosk.config.baseEntity.BaseTimeEntity;
-import com.bttf.queosk.dto.TableStatus;
+import com.bttf.queosk.dto.enumerate.TableStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
