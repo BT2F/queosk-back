e6ecf275da4071778ca1a82982280a655e82b072
peppone-choi <peppone.choi@gmail.com>
2023-08-18 21:11:55 +0900

    chore: 어노테이션 제거

--- queosk/src/main/java/com/bttf/queosk/dto/restaurantDto/RestaurantUpdatePasswordForm.java
+++ queosk/src/main/java/com/bttf/queosk/dto/restaurantDto/RestaurantUpdatePasswordForm.java
@@ -1,13 +1,9 @@
 package com.bttf.queosk.dto.restaurantDto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class RestaurantUpdatePasswordForm {
     private String oldPassword;
