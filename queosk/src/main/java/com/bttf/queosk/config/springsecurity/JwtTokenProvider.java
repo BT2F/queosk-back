4d06d3afe26963b1e5b8a687dc47d732e32cda0b
HyunsooZo <bzhs1992@icloud.com>
2023-08-19 21:48:47 +0900

    Fix: AccessToken 24시간 , RefreshToken 360시간(15일) 로 만료시간 수정

--- queosk/src/main/java/com/bttf/queosk/config/springsecurity/JwtTokenProvider.java
+++ queosk/src/main/java/com/bttf/queosk/config/springsecurity/JwtTokenProvider.java
@@ -1,7 +1,7 @@
 package com.bttf.queosk.config.springsecurity;
 
 import com.bttf.queosk.dto.tokendto.TokenDto;
-import com.bttf.queosk.model.userModel.UserRole;
+import com.bttf.queosk.enumerate.UserRole;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -28,9 +28,6 @@ public class JwtTokenProvider {
 
     private final UserDetailsService userDetailsService;
 
-    @Value("${jwt.secretKey}")
-    private String secretKey;
-
     @Value("${jwt.tokenIssuer}")
     private String issuer;
     private SecretKey key;
@@ -49,7 +46,8 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuer(issuer)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + (60 * 60 * 1000)))
+                // 유효기간 1일 (24시간)
+                .setExpiration(new Date(System.currentTimeMillis() + (24 * 60 * 60 * 1000)))
                 .signWith(key)
                 .compact();
     }
@@ -59,7 +57,8 @@ public class JwtTokenProvider {
                 .claim("userRole", UserRole.ROLE_USER.getRoleName())
                 .setIssuer(issuer)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + (60 * 60 * 1000)))
+                //유효기간 15일 (24시간 *15 )
+                .setExpiration(new Date(System.currentTimeMillis() + (15 * 24 * 60 * 60 * 1000)))
                 .signWith(key)
                 .compact();
     }
