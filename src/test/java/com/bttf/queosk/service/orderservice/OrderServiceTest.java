5ed22f98127aa3e55f892aec9f7ecf8403fd2b80
peppone-choi <peppone.choi@gmail.com>
2023-08-28 17:49:07 +0900

    Merge: 테스트 다시 수정 (#71)

--- src/test/java/com/bttf/queosk/service/orderservice/OrderServiceTest.java
+++ src/test/java/com/bttf/queosk/service/orderservice/OrderServiceTest.java
@@ -42,58 +42,58 @@ class OrderServiceTest {
     @Mock
     private TableRepository tableRepository;
 
-//    @Test
-//    public void createOrder_success() {
-//        // given
-//
-//        Long restaurantId = 1L;
-//        Long menuId = 1L;
-//        String userEmail = "a@x.com";
-//        Long tableId = 1L;
-//
-//        Restaurant restaurant = Restaurant.builder()
-//                .id(1L)
-//                .build();
-//
-//        Menu menu1 =
-//                Menu.builder()
-//                        .id(1L)
-//                        .name("menu1")
-//                        .price(1000L)
-//                        .status(ON_SALE)
-//                        .restaurantId(restaurantId)
-//                        .build();
-//
-//        Table table = Table.builder()
-//                .id(1L)
-//                .status(TableStatus.OPEN)
-//                .restaurantId(1L)
-//                .build();
-//
-//        User user = User.builder()
-//                .id(1L)
-//                .email(userEmail)
-//                .build();
-//
-//        CreateOrderForm createOrderForm = CreateOrderForm.builder()
-//                .menuId(menuId)
-//                .tableId(tableId)
-//                .restaurantId(restaurantId)
-//                .count(1)
-//                .build();
-//
-//        given(restaurantRepository.findById(1L)).willReturn(Optional.of(restaurant));
-//        given(menuRepository.findById(1L)).willReturn(Optional.of(menu1));
-//        given(tableRepository.findById(1L)).willReturn(Optional.of(table));
-//        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-//        // when
-//
-////        orderService.createOrder(createOrderForm, 1L);
-//
-//        // then
-//
-////        verify(orderRepository, times(1)).save(any(Order.class));
-//    }
+    @Test
+    public void createOrder_success() {
+        // given
+
+        Long restaurantId = 1L;
+        Long menuId = 1L;
+        String userEmail = "a@x.com";
+        Long tableId = 1L;
+
+        Restaurant restaurant = Restaurant.builder()
+                .id(1L)
+                .build();
+
+        Menu menu1 =
+                Menu.builder()
+                        .id(1L)
+                        .name("menu1")
+                        .price(1000L)
+                        .status(ON_SALE)
+                        .restaurantId(restaurantId)
+                        .build();
+
+        Table table = Table.builder()
+                .id(1L)
+                .status(TableStatus.OPEN)
+                .restaurantId(1L)
+                .build();
+
+        User user = User.builder()
+                .id(1L)
+                .email(userEmail)
+                .build();
+
+        CreateOrderForm createOrderForm = CreateOrderForm.builder()
+                .menuId(menuId)
+                .tableId(tableId)
+                .restaurantId(restaurantId)
+                .count(1)
+                .build();
+
+        given(restaurantRepository.findById(1L)).willReturn(Optional.of(restaurant));
+        given(menuRepository.findByIdAndRestaurantId(1L, 1L)).willReturn(Optional.of(menu1));
+        given(tableRepository.findById(1L)).willReturn(Optional.of(table));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        // when
+
+        orderService.createOrder(createOrderForm, 1L);
+
+        // then
+
+        verify(orderRepository, times(1)).save(any(Order.class));
+    }
 
     @Test
     public void updateOrderStatus_success() {
