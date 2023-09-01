a34c649e48b8c54cb48919a3a48247abf2b3b9e0
peppone-choi <peppone.choi@gmail.com>
2023-09-01 20:28:28 +0900

    Fix: Redis 수정

--- src/main/java/com/bttf/queosk/config/RedisConfig.java
+++ src/main/java/com/bttf/queosk/config/RedisConfig.java
@@ -1,29 +1,44 @@
 package com.bttf.queosk.config;
 
 import com.bttf.queosk.entity.RedisQueue;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.ReadFrom;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+import java.util.List;
+
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
+    @Value("${spring.redis.cluster.nodes}")
+    private List<String> clusterNodes;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        clusterNodes.forEach(
+                node -> {
+                    String[] url = node.split(":");
+                    redisClusterConfiguration.clusterNode(url[0], Integer.parseInt(url[1]));
+                }
+        );
+
+
+        return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
     @Bean
@@ -37,5 +52,7 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
+
 
 }
