7d6adf528c8166744f1b206a3bb42725eb773a05
HyunsooZo <bzhs1992@icloud.com>
2023-09-10 05:03:50 +0900

    Feat: api 접근권한 설정 수정

--- src/main/java/com/bttf/queosk/config/SecurityConfig.java
+++ src/main/java/com/bttf/queosk/config/SecurityConfig.java
@@ -27,13 +27,13 @@ public class SecurityConfig {
                                 "/api/restaurants/*/queue",      // 사용자 웨이팅 등록
                                 "/api/restaurants/*/user/queue", // 사용자 웨이팅 취소
                                 "/api/users",                    // 사용자 본인 정보관련
-                                "/api/users/password/*"          // 사용자 비밀번호 관련
+                                "/api/users/password/change"     // 사용자 비밀번호 변경
                         ).hasRole("USER")
                         .antMatchers(
                                 "/api/restaurants/menus/*",        // 메뉴 관리 api
                                 "/api/restaurant/order/*",         // 매장 주문관리
                                 "/api/restaurants/queue",          // 매장 큐 관리
-                                "/api/restaurants/password/*",     // 매장 비밀번호 변경
+                                "/api/restaurants/password/change",// 매장 비밀번호 변경
                                 "/api/restaurants/image",          // 매장 이미지 추가
                                 "/api/restaurants",                // 매장 정보 관리
                                 "/api/restaurants/settlement/*",   // 매장 정산 관리
