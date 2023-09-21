0e7004f80defa4cd1e3158d07058b25f767655fe
HyunsooZo <bzhs1992@icloud.com>
2023-09-21 11:46:48 +0700

    Refactor: 중복코드 방지를 위해 이너클래스를 사용하던 KakaoLoginForm 분리
    KakaoLoginForm.Request -> KakaoLoginRequest
    KakaoLoginForm.Response -> KakaoLoginResponse

--- src/main/java/com/bttf/queosk/controller/KakaoLoginController.java
+++ src/main/java/com/bttf/queosk/controller/KakaoLoginController.java
@@ -1,6 +1,7 @@
 package com.bttf.queosk.controller;
 
-import com.bttf.queosk.dto.KakaoLoginForm;
+import com.bttf.queosk.dto.KakaoLoginRequest;
+import com.bttf.queosk.dto.KakaoLoginResponse;
 import com.bttf.queosk.dto.UserSignInDto;
 import com.bttf.queosk.service.KakaoLoginService;
 import io.swagger.annotations.Api;
@@ -27,9 +28,9 @@ public class KakaoLoginController {
     @PostMapping("/signin")
     @ApiOperation(value = "카카오톡 소셜 로그인 코드를 사용하여 로그인진행",
             notes = "카카오톡 로그인 URL을 통해 얻은 code 및 데이터를 사용하여 로그인 또는 회원가입을 진행합니다.")
-    public ResponseEntity<KakaoLoginForm.Response> kakaoLoginCallback(
+    public ResponseEntity<KakaoLoginResponse> kakaoLoginCallback(
             HttpServletRequest request,
-            @Valid @RequestBody KakaoLoginForm.Request kaKaoLoginRequest) {
+            @Valid @RequestBody KakaoLoginRequest kaKaoLoginRequest) {
 
         String host = request.getHeader("Host");
 
@@ -38,6 +39,6 @@ public class KakaoLoginController {
             kakaoLoginService.getUserInfoFromKakaoTest(kaKaoLoginRequest):
             kakaoLoginService.getUserInfoFromKakao(kaKaoLoginRequest);
 
-        return ResponseEntity.status(OK).body(KakaoLoginForm.Response.of(userSignInDto));
+        return ResponseEntity.status(OK).body(KakaoLoginResponse.of(userSignInDto));
     }
 }
