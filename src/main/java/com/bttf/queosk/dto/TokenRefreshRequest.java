1207ade2ce8ab18ed1ab610a0e31091a3adc2abd
HyunsooZo <bzhs1992@icloud.com>
2023-09-21 12:48:27 +0700

    Refactor: 중복코드 방지를 위해 이너클래스를 사용하던 Token 관련 Dto 분리
    TokenRefreshRequest.java
    TokenRefreshResponse.java

---
+++ src/main/java/com/bttf/queosk/dto/TokenRefreshRequest.java
@@ -0,0 +1,16 @@
+package com.bttf.queosk.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "토큰재발급 Request")
+public class TokenRefreshRequest {
+    private String refresh_token;
+}
