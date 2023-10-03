71831e195a40bb671b13697b3a2f356c5c2a984f
HyunsooZo <bzhs1992@icloud.com>
2023-10-04 06:54:28 +0900

    Fix: #220 비밀번호 초기화 403 예외 이슈 해결

--- src/main/java/com/bttf/queosk/config/SecurityConfig.java
+++ src/main/java/com/bttf/queosk/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                                 "/**/refresh",                    // 토큰갱신 관련
                                 "/**/callback",                   // 기타 콜백
                                 "/**/users/check",                // 가입전 이메일 중복확인
-                                "/password/reset",                // 비로그인 비밀번호 리셋
+                                "/**/password/reset",                // 비로그인 비밀번호 리셋
                                 "/api/restaurants/coord",         // 매장 조회 (좌표)
                                 "/api/restaurants/keyword",         // 매장 조회 (키워드)
                                 "/api/restaurants/*/menus",       // 매장 메뉴 조회
