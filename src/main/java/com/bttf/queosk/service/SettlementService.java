bd11bd5906e23eb8093520591b39af95a8bddc47
HyunsooZo <bzhs1992@icloud.com>
2023-10-06 20:01:38 +0900

    Feat: SettlementResponseForm 에 각 메뉴의 가격 반환하도록 수정

--- src/main/java/com/bttf/queosk/service/SettlementService.java
+++ src/main/java/com/bttf/queosk/service/SettlementService.java
@@ -1,7 +1,7 @@
 package com.bttf.queosk.service;
 
+import com.bttf.queosk.dto.MenuItemDto;
 import com.bttf.queosk.dto.SettlementDto;
-import com.bttf.queosk.entity.MenuItem;
 import com.bttf.queosk.entity.Order;
 import com.bttf.queosk.entity.Settlement;
 import com.bttf.queosk.repository.MenuItemRepository;
@@ -10,13 +10,11 @@ import com.bttf.queosk.repository.SettlementRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,49 +26,49 @@ public class SettlementService {
 
     private final MenuItemRepository menuItemRepository;
 
-    public SettlementDto SettlementGet(Long restaurantId,
-                                       LocalDateTime from,
-                                       LocalDateTime to) {
-
-        Map<String, Integer> menuList = new HashMap<>();
-        Long total = 0L;
-
+    public SettlementDto SettlementGet(Long restaurantId, LocalDateTime from, LocalDateTime to) {
         List<Order> orderByRestaurantInDateRange = orderRepository
                 .findOrderByRestaurantInDateRange(restaurantId, from, to);
 
-        for (Order order : orderByRestaurantInDateRange) {
-            List<MenuItem> allByOrderId = menuItemRepository.findAllByOrderId(order.getId());
-            for (MenuItem menuItem : allByOrderId) {
-                menuList.put(menuItem.getMenu().getName(), menuList.getOrDefault(menuItem.getMenu().getName(), 0) + menuItem.getCount());
-                total += menuItem.getCount() * menuItem.getMenu().getPrice();
-            }
-        }
+        //메뉴 별 가격 반환을 위한 Value 에 MenuItemDto 사용
+        Map<String, MenuItemDto> menuList = orderByRestaurantInDateRange.stream()
+                .flatMap(order -> menuItemRepository.findAllByOrderId(order.getId()).stream())
+                .collect(HashMap::new,
+                        (map, menuItem) -> map.merge(
+                                menuItem.getMenu().getName(),
+                                new MenuItemDto(menuItem.getMenu().getPrice(), menuItem.getCount()),
+                                (existing, replacement) -> existing.addCount(replacement.getCount())
+                        ),
+                        (map1, map2) -> map2.forEach((key, value) -> map1.merge(
+                                key,
+                                value,
+                                (existing, replacement) -> existing.addCount(replacement.getCount())
+                        ))
+                );
 
-        List<SettlementDto.OrderdMenu> orderdMenuList = new ArrayList<>();
-        for (Map.Entry<String, Integer> menu : menuList.entrySet()) {
-            SettlementDto.OrderdMenu orderdMenu = new SettlementDto.OrderdMenu(menu.getKey(), menu.getValue());
-            orderdMenuList.add(orderdMenu);
-        }
+        List<SettlementDto.OrderdMenu> orderdMenuList = menuList.entrySet().stream()
+                .map(entry -> new SettlementDto.OrderdMenu(
+                        entry.getKey(), entry.getValue().getMenuPrice(), entry.getValue().getCount())
+                )
+                .collect(Collectors.toList());
+
+        long total = menuList.values().stream()
+                .mapToLong(menuItemDto -> menuItemDto.getCount() * menuItemDto.getMenuPrice())
+                .sum();
 
         return SettlementDto.of(orderdMenuList, total);
-
     }
 
     public Long periodSettlementPriceGet(Long restaurantId,
-                                         LocalDateTime to,
-                                         LocalDateTime from) {
-        LocalDateTime startDateTime = LocalDateTime.of(LocalDate.from(from), LocalTime.MIN);
-        LocalDateTime endDateTime = LocalDateTime
-                .of(LocalDate.from(to.plusDays(1)), LocalTime.MIN)
-                .minusNanos(1);
-        List<Settlement> settlementList =
-                settlementRepository.findSettlementsByRestaurantInDateRange(restaurantId, startDateTime, endDateTime);
+                                         LocalDateTime from,
+                                         LocalDateTime to) {
 
-        long totalPrice = settlementList.stream()
+        List<Settlement> settlementList =
+                settlementRepository.findSettlementsByRestaurantInDateRange(restaurantId, from, to);
+
+        return settlementList.stream()
                 .mapToLong(Settlement::getPrice)
                 .sum();
-
-        return totalPrice;
     }
 
 }
