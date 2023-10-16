2a35cb9dc5b94f102653ceef286b2191d2b33090
peppone-choi <peppone.choi@gmail.com>
2023-10-16 10:44:52 +0900

    Fix: TableDto에 Constructor 추가 #234번 이슈의 원인으로 확인 된 TableDto에 Constructor를 추가하였습니다

--- src/main/java/com/bttf/queosk/dto/TableDto.java
+++ src/main/java/com/bttf/queosk/dto/TableDto.java
@@ -4,12 +4,16 @@ package com.bttf.queosk.dto;
 import com.bttf.queosk.entity.Table;
 import com.bttf.queosk.enumerate.TableStatus;
 import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @ApiModel(value = "Table Dto")
+@NoArgsConstructor
+@AllArgsConstructor
 public class TableDto {
     private String tableName;
     private Long tableId;
