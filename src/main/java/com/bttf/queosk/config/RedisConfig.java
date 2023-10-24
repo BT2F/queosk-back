ed728958fef8da58a587b3559dffc552d8385e4c
peppone-choi <peppone.choi@gmail.com>
2023-10-24 20:47:37 +0900

    Feat: ReviewDto, ReviewResponseForm 수정
    - ReviewService, ReviewDto, ReviewResponseForm 코드 수정 및 리팩토링
    - ReviewController 코드 수정
    - 관련 테스트 코드 수정 후 테스트
    - API 테스트

--- src/main/java/com/bttf/queosk/config/RedisConfig.java
+++ src/main/java/com/bttf/queosk/config/RedisConfig.java
@@ -27,35 +27,35 @@ public class RedisConfig {
 
 //    로컬에서 테스트 시 아래를 활성화 하여야 함
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
-
-// 로컬에서 테스트시 아래를 비활성화 하여야 함
-
-//    @Value("${spring.redis.cluster.nodes}")
-//    private List<String> clusterNodes;
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+//
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
 //
 //    @Bean
 //    public RedisConnectionFactory redisConnectionFactory() {
-//        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
-//        clusterNodes.forEach(
-//                node -> {
-//                    String[] url = node.split(":");
-//                    redisClusterConfiguration.clusterNode(url[0], Integer.parseInt(url[1]));
-//                }
-//        );
-//
-//
-//        return new LettuceConnectionFactory(redisClusterConfiguration);
+//        return new LettuceConnectionFactory(redisHost, redisPort);
 //    }
+
+// 로컬에서 테스트시 아래를 비활성화 하여야 함
+
+    @Value("${spring.redis.cluster.nodes}")
+    private List<String> clusterNodes;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        clusterNodes.forEach(
+                node -> {
+                    String[] url = node.split(":");
+                    redisClusterConfiguration.clusterNode(url[0], Integer.parseInt(url[1]));
+                }
+        );
+
+
+        return new LettuceConnectionFactory(redisClusterConfiguration);
+    }
 
     //  여기까지 비활성화
 
